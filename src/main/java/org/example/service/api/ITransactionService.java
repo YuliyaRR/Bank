7436990185cd638f129e8df6a183abbd8bf77571
Yuliya Romashko <ramashkay@gmail.com>
2023-09-01 12:18:30 +0300

    Added: method for saving monthly interest transactions

--- src/main/java/org/example/service/api/ITransactionService.java
+++ src/main/java/org/example/service/api/ITransactionService.java
@@ -2,6 +2,9 @@ package org.example.service.api;
 
 import org.example.core.dto.Transaction;
 
+import java.util.List;
+
 public interface ITransactionService {
     void saveTransaction(Transaction transaction);
+    void saveMonthlyInterestTransactions(List<Transaction> transactions);
 }
--- src/main/java/org/example/service/impl/TransactionService.java
+++ src/main/java/org/example/service/impl/TransactionService.java
@@ -6,13 +6,29 @@ import org.example.dao.repositories.api.ITransactionRepository;
 import org.example.dao.entity.TransactionEntity;
 import org.example.service.api.ITransactionService;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public class TransactionService implements ITransactionService {
     private final ITransactionRepository transactionRepository;
 
     @Override
     public void saveTransaction(Transaction transaction) {
-        TransactionEntity entity = TransactionEntity.builder()
+        transactionRepository.saveTransaction(convertToEntity(transaction));
+    }
+
+    @Override
+    public void saveMonthlyInterestTransactions(List<Transaction> transactions) {
+        transactionRepository.saveMonthlyInterestTransactions(
+                transactions.stream()
+                .map(transaction -> convertToEntity(transaction))
+                .toList()
+        );
+
+    }
+
+    private TransactionEntity convertToEntity(Transaction transaction) {
+        return TransactionEntity.builder()
                 .setId(transaction.getId())
                 .setCurrency(transaction.getCurrency().name())
                 .setDate(transaction.getDate())
@@ -21,7 +37,5 @@ public class TransactionService implements ITransactionService {
                 .setSum(transaction.getSum())
                 .setType(transaction.getType().name())
                 .build();
-
-        transactionRepository.save(entity);
     }
 }
