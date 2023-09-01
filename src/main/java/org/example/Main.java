5dd9b7ce0f918a8175cac16503a128c8ef612959
Yuliya Romashko <ramashkay@gmail.com>
2023-09-01 12:51:53 +0300

    Added: ScheduledExecutorService to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,32 +2,37 @@ package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.example.dao.api.IAccountRepository;
-import org.example.dao.api.ITransactionRepository;
-import org.example.dao.ds.api.IDataSourceWrapper;
 import org.example.dao.ds.factory.DataSourceC3POSingleton;
-import org.example.dao.factory.AccountRepositorySingleton;
-import org.example.dao.factory.TransactionRepositorySingleton;
-import java.beans.PropertyVetoException;
+import org.example.dao.repositories.factory.AccountRepositorySingleton;
+import org.example.dao.repositories.factory.TransactionRepositorySingleton;
+import org.example.service.api.IAccountService;
+import org.example.service.api.ITransactionService;
+import org.example.service.factory.AccountServiceSingleton;
+import org.example.service.factory.TransactionServiceSingleton;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException, PropertyVetoException {
+    public static void main(String[] args) throws IOException{
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-
         Properties properties = mapper.readValue(new File("src/main/resources/application.yml"), Properties.class);
 
         DataSourceC3POSingleton.setProperties(properties);
-        IDataSourceWrapper dataSource = DataSourceC3POSingleton.getInstance();
-
         AccountRepositorySingleton.setProperties(properties);
-        IAccountRepository repository = AccountRepositorySingleton.getInstance();
-
         TransactionRepositorySingleton.setProperties(properties);
-        ITransactionRepository transactionRepository = TransactionRepositorySingleton.getInstance();
 
+        IAccountService accountService = AccountServiceSingleton.getInstance();
+        ITransactionService transactionService = TransactionServiceSingleton.getInstance();
+
+        ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+
+        scheduledExecutor.scheduleAtFixedRate(() -> accountService.checkTheNeedToCalculateInterest(),
+                1000, 30000, TimeUnit.MILLISECONDS);
     }
 }
