7bced95c98740cd367fa916dc7d00c17f9eb046e
Yuliya Romashko <ramashkay@gmail.com>
2023-09-04 02:42:04 +0300

    Added: CRUD-method to api and impl BankService

--- src/main/java/org/example/service/api/IBankService.java
+++ src/main/java/org/example/service/api/IBankService.java
@@ -2,8 +2,14 @@ package org.example.service.api;
 
 import org.example.core.dto.Bank;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IBankService {
     Bank getBankByAccount(UUID account);
+    List<Bank> getAllBanks();
+    void createBank(Bank bank);
+    void updateBank(UUID uuid, Bank bank);
+    void deleteBank(UUID uuid);
+
 }
--- src/main/java/org/example/service/impl/BankService.java
+++ src/main/java/org/example/service/impl/BankService.java
@@ -6,6 +6,7 @@ import org.example.dao.entity.BankEntity;
 import org.example.dao.repositories.api.IBankRepository;
 import org.example.service.api.IBankService;
 
+import java.util.List;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -19,6 +20,53 @@ public class BankService implements IBankService {
     @Override
     public Bank getBankByAccount(UUID account) {
         BankEntity entity = bankRepository.getBankByAccount(account);
+        return new Bank(entity.getId(), entity.getName());
+    }
+
+    /**
+     * Метод возвращает информацию обо всех банках
+     * @return список банков
+     */
+    @Override
+    public List<Bank> getAllBanks() {
+        List<BankEntity> allBanks = bankRepository.getAllBanks();
+        return allBanks.stream()
+                .map(bankEntity -> convertEntityToDto(bankEntity))
+                .toList();
+    }
+
+    /** Метод передает информацию о банке для сохранения в БД
+     * @param bank объект-источник информации о новом банке, на данный момент это имя нового банка
+     */
+    @Override
+    public void createBank(Bank bank) {
+        bankRepository.saveBank(new BankEntity(UUID.randomUUID(), bank.getName()));
+    }
+
+    /**
+     * Метод передает данные для обновления информации о банке на БД
+     * @param uuid идентификатор банка, информация о котором будет обновлена
+     * @param bank объект-источник информации о новом банке, на данный момент это новое имя банка
+     */
+    @Override
+    public void updateBank(UUID uuid, Bank bank) {
+        bankRepository.updateBank(uuid, new BankEntity(uuid, bank.getName()));
+    }
+
+    /**
+     * Метод передает данные для удаления банка из БД
+     * @param uuid идентификатор банка, который будет удален
+     */
+    @Override
+    public void deleteBank(UUID uuid) {
+        bankRepository.deleteBank(uuid);
+    }
+
+    /** Мотод конвертирует BankEntity в Bank
+     * @param entity объект-источник
+     * @return Bank
+     */
+    private Bank convertEntityToDto(BankEntity entity) {
         return new Bank(entity.getId(), entity.getName());
     }
 }
