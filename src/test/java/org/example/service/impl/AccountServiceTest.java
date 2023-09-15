2a399383c4fc003b7a8f5627df17100462497967
Yuliya Romashko <ramashkay@gmail.com>
2023-09-15 23:40:22 +0300

    Added: tests for transferMoney() in AccountService

--- src/test/java/org/example/service/impl/AccountServiceTest.java
+++ src/test/java/org/example/service/impl/AccountServiceTest.java
@@ -119,6 +119,8 @@ public class AccountServiceTest {
 
         RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.addMoney(in));
         assertEquals(EXCEPTION_WRONG_CURRENCY, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(Mockito.any());
     }
 
     @Test
@@ -189,6 +191,8 @@ public class AccountServiceTest {
 
         RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.withdrawalMoney(in));
         assertEquals(EXCEPTION_WRONG_CURRENCY, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(Mockito.any());
     }
 
     @Test
@@ -202,5 +206,122 @@ public class AccountServiceTest {
 
         RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.withdrawalMoney(in));
         assertEquals(EXCEPTION_BALANCE, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(Mockito.any());
+    }
+
+    @Test
+    public void transferMoneyWhenAllDataIsValid() {
+        in.setType(TransactionType.WAGE);
+
+        AccountEntity accountTo = AccountEntity.builder().setBalance(1000).setCurrency("USD").build();
+        AccountEntity accountFrom = AccountEntity.builder().setBalance(20000).setCurrency("USD").build();
+
+        when(accountRepository.checkAccount(in.getAccountFrom())).thenReturn(accountFrom);
+        when(accountRepository.checkAccount(in.getAccountTo())).thenReturn(accountTo);
+        when(accountRepository.updateBalanceCashlessPayments(in)).thenAnswer((Answer<Transaction>) invocation -> (Transaction) invocation.getArgument(0));
+
+        Check check = accountService.transferMoney(in);
+
+        Mockito.verify(accountRepository, Mockito.times(2)).checkAccount(Mockito.any(UUID.class));
+        Mockito.verify(accountRepository, Mockito.times(1)).updateBalanceCashlessPayments(in);
+        Mockito.verify(bankService, Mockito.times(2)).getBankByAccount(Mockito.any(UUID.class));
+        Mockito.verify(publisher, Mockito.times(1)).notify(Mockito.any(CheckEvent.class));
+
+        assertNotNull(check);
+        assertNotNull(check.getNumber());
+        assertEquals(in.getType(), check.getTransactionType());
+        assertEquals(in.getAccountFrom(), check.getAccountFrom());
+        assertEquals(in.getAccountTo(), check.getAccountTo());
+        assertEquals(in.getSum(), check.getSum());
+        assertEquals(in.getCurrency(), check.getCurrency());
+    }
+
+    @Test
+    public void transferMoneyWhenTransactionTypeWrongThenThrowException(){
+        in.setType(TransactionType.CASH_REPLENISHMENT);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+        assertEquals(EXCEPTION_WRONG_TYPE, exception.getMessage());
+    }
+
+    @Test
+    public void transferMoneyWhenTransactionSumWrongThenThrowException(){
+        in.setType(TransactionType.WAGE);
+
+        in.setSum(-100);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+        assertEquals(EXCEPTION_WRONG_SUM, exception.getMessage());
+    }
+
+    @Test
+    public void transferMoneyWhenAccountFromWasNotFoundThenThrowException(){
+        in.setType(TransactionType.WAGE);
+
+        when(accountRepository.checkAccount(in.getAccountFrom())).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountFrom());
+    }
+
+    @Test
+    public void transferMoneyWhenCurrencyAccountFromWrongThenThrowException() {
+        in.setType(TransactionType.WAGE);
+        in.setCurrency(Currency.RUB);
+        AccountEntity accountFrom = AccountEntity.builder().setBalance(20000).setCurrency("USD").build();
+
+        when(accountRepository.checkAccount(in.getAccountFrom())).thenReturn(accountFrom);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+        assertEquals(EXCEPTION_WRONG_CURRENCY, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountFrom());
+    }
+
+    @Test
+    public void transferMoneyWhenInsufficientBalanceThenThrowException() {
+        in.setType(TransactionType.WAGE);
+
+        AccountEntity fromDao = AccountEntity.builder().setBalance(50).setCurrency("USD").build();
+
+        when(accountRepository.checkAccount(any(UUID.class))).thenReturn(fromDao);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+        assertEquals(EXCEPTION_BALANCE, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountFrom());
+    }
+
+    @Test
+    public void transferMoneyWhenAccountToWasNotFoundThenThrowException(){
+        in.setType(TransactionType.WAGE);
+        AccountEntity accountFrom = AccountEntity.builder().setBalance(20000).setCurrency("USD").build();
+
+        when(accountRepository.checkAccount(in.getAccountFrom())).thenReturn(accountFrom);
+        when(accountRepository.checkAccount(in.getAccountTo())).thenThrow(RuntimeException.class);
+
+        assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountFrom());
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountTo());
+    }
+
+    @Test
+    public void transferMoneyWhenCurrencyAccountToWrongThenThrowException() {
+        in.setType(TransactionType.WAGE);
+        in.setCurrency(Currency.RUB);
+        AccountEntity accountFrom = AccountEntity.builder().setBalance(20000).setCurrency("RUB").build();
+        AccountEntity accountTo= AccountEntity.builder().setBalance(1000).setCurrency("USD").build();
+
+        when(accountRepository.checkAccount(in.getAccountFrom())).thenReturn(accountFrom);
+        when(accountRepository.checkAccount(in.getAccountTo())).thenReturn(accountTo);
+
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> accountService.transferMoney(in));
+        assertEquals(EXCEPTION_WRONG_CURRENCY, exception.getMessage());
+
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountFrom());
+        Mockito.verify(accountRepository, Mockito.times(1)).checkAccount(in.getAccountTo());
     }
 }
