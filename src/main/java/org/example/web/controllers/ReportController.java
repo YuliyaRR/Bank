4b7db1da85d57c4ac48d3f2f4133bb7bd89fb49d
Yuliya Romashko <ramashkay@gmail.com>
2023-09-26 00:08:07 +0300

    Added: opportunity to get money statement

--- src/main/java/org/example/web/controllers/ReportController.java
+++ src/main/java/org/example/web/controllers/ReportController.java
@@ -1,8 +1,9 @@
 package org.example.web.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.example.core.dto.AccountStatement;
-import org.example.core.dto.Duration;
+import org.example.core.dto.*;
+import org.example.core.dto.docs.AccountStatement;
+import org.example.core.dto.docs.MoneyStatement;
 import org.example.service.api.IReportService;
 import org.example.service.factory.ObjectMapperHelperSingleton;
 import org.example.service.factory.ReportServiceSingleton;
@@ -14,6 +15,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @WebServlet(name = "ReportController", urlPatterns = "/reports")
@@ -22,6 +24,9 @@ public class ReportController extends HttpServlet {
     private final ObjectMapper mapper;
     private final static String REPORT_DURATION = "duration";
     private final static String REPORT_ACCOUNT = "account";
+    private final static String REPORT_TYPE = "report_type";
+    private final static String REPORT_DATE_FROM = "from";
+    private final static String REPORT_DATE_TO = "to";
 
 
     public ReportController() {
@@ -37,13 +42,27 @@ public class ReportController extends HttpServlet {
         PrintWriter writer = resp.getWriter();
 
         try {
-            Duration duration = Duration.valueOf(req.getParameter(REPORT_DURATION));
             UUID uuid = UUID.fromString(req.getParameter(REPORT_ACCOUNT));
+            ReportType reportType = ReportType.valueOf(req.getParameter(REPORT_TYPE));
 
-            AccountStatement accountStatement = reportService.getAccountStatement(uuid, duration);
+            if (reportType.equals(ReportType.ACCOUNT_STATEMENT)) {
+                Duration duration = Duration.valueOf(req.getParameter(REPORT_DURATION));
 
-            writer.write(mapper.writeValueAsString(accountStatement));
+                AccountStatement accountStatement = reportService.getAccountStatement(uuid, duration);
+                writer.write(mapper.writeValueAsString(accountStatement));
 
+            } else if (reportType.equals(ReportType.MONEY_STATEMENT)) {
+                String parameterDateFrom = req.getParameter(REPORT_DATE_FROM);
+                LocalDate dateFrom = parseDate(parameterDateFrom);
+
+                String parameterDateTo = req.getParameter(REPORT_DATE_TO);
+                LocalDate dateTo = parseDate(parameterDateTo);
+
+                Period period = new Period(dateFrom, dateTo);
+
+                MoneyStatement moneyStatement = reportService.getMoneyStatement(uuid, period);
+                writer.write(mapper.writeValueAsString(moneyStatement));
+            }
         } catch (RuntimeException e) {
             if (e.getCause() != null) {
                 writer.write(e.getMessage() + ": " + e.getCause());
@@ -51,8 +70,12 @@ public class ReportController extends HttpServlet {
                 writer.write(e.getMessage());
             }
         }
+    }
 
-
-
+    private LocalDate parseDate(String date) {
+        if (date == null) {
+            throw new IllegalArgumentException("The date of the reporting period is not specified");
+        }
+        return LocalDate.parse(date);
     }
 }
